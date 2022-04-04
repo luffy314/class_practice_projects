3a944d908b45fecf31e341198bbd62c328e31f21
Luffy <95117871+luffy314@users.noreply.github.com>
2022-04-04 02:30:23 -0400

    updating java package

---
+++ src/day40_arraylist/Practice.java
@@ -0,0 +1,13 @@
+package day40_arraylist;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Practice {
+    public static void main(String[] args) {
+
+
+
+
+    }
+}
---
+++ src/day40_arraylist_homework/ReverseAll.java
@@ -0,0 +1,4 @@
+package day40_arraylist_homework;
+
+public class ReverseAll {
+}
