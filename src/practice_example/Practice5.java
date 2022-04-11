4dd22282ff09c5e6ac60769443af427a510f496c
Luffy <95117871+luffy314@users.noreply.github.com>
2022-04-10 21:12:03 -0400

    updating java package

--- src/practice_example/Practice5.java
+++ src/practice_example/Practice5.java
@@ -2,6 +2,9 @@ package practice_example;
 
 public class Practice5 {
     public static void main(String[] args) {
+        String a = "apples3";
+        int b=12345;
+        System.out.println(a.indexOf("apples34"));
 
     }
 }
