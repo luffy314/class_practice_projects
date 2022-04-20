7fcbd636f78a38eeac68e99e9db52a9a16946cd4
Luffy <95117871+luffy314@users.noreply.github.com>
2022-04-20 02:59:21 -0400

    updating java package

---
+++ src/day49_inheritance/Zoo.java
@@ -0,0 +1,14 @@
+package day49_inheritance;
+
+public class Zoo {
+    public static void main(String[] args) {
+        Animal animal = new Animal();
+        animal.species = "Generic Animal";
+        animal.numberOfLegs = 0;
+        animal.walk();
+
+        Dog dog = new Dog();
+        dog.species = "Dog";
+        dog.numberOfLegs = 4;
+    }
+}
