776ab9660ba504fa71f569cecfefa7790415efc8
Luffy <95117871+luffy314@users.noreply.github.com>
2022-05-10 22:14:08 -0400

    updating java package

---
+++ src/day56_polymorphism/Downloadable.java
@@ -0,0 +1,12 @@
+package day56_polymorphism;
+
+/*
+Create an interface Downloadable
+    - Declare an abstract methods:
+        download();
+ */
+public interface Downloadable {
+
+    void download();
+
+}
