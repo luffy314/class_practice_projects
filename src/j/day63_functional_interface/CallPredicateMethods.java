7f43f8561001b2a421548573a739c512e639d906
Luffy <95117871+luffy314@users.noreply.github.com>
2022-05-28 16:20:49 -0400

    updating java package

---
+++ src/j/day63_functional_interface/CallPredicateMethods.java
@@ -0,0 +1,19 @@
+package j.day63_functional_interface;
+
+import java.util.ArrayList;
+
+public class CallPredicateMethods {
+    public static void main(String[] args) {
+
+        boolean b = UsePredicate.isPalindrome.test("racecar");
+        System.out.println(b);
+
+        System.out.println(UsePredicate.isPalindrome.test("abc"));
+
+        System.out.println(UsePredicate.isPrime.test(5));
+
+        System.out.println(UsePredicate.isPrime.test(10));
+
+
+    }
+}
---
+++ src/j/day63_functional_interface/Test.java
@@ -0,0 +1,12 @@
+package j.day63_functional_interface;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Test {
+    public static void main(String[] args) {
+
+
+
+    }
+}
