7463e10c1d2c8b183ea2c41503a55d7b19b8437c
Luffy <95117871+luffy314@users.noreply.github.com>
2022-05-29 09:24:50 -0400

    updating java package

--- src/j/day63_functional_interface/Test.java
+++ src/j/day63_functional_interface/Test.java
@@ -6,6 +6,20 @@ import java.util.Arrays;
 public class Test {
     public static void main(String[] args) {
 
+    String one= "cool";
+    String reverse = new StringBuilder("wolf").reverse().toString();
+        System.out.println(reverse);
+
+        StringBuilder two= new StringBuilder("chicken");
+        System.out.println(two);
+
+        int[] three={2,3,4};
+        ArrayList<int[]> four= new ArrayList<>(Arrays.asList(three));
+        System.out.println(four);
+
+        int[] ints = new int[] {1,2,3,4,5};
+
+        System.out.println(Arrays.toString(ints));
 
 
     }
